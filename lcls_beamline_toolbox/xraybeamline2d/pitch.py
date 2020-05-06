0b3622166bba5c6cb2e4a242549d935ef5e65ea4
Matthew H. Seaberg <seaberg@slac.stanford.edu>
2020-05-06 15:58:00 -0700

    minor changes

--- lcls_beamline_toolbox/xraybeamline2d/pitch.py
+++ lcls_beamline_toolbox/xraybeamline2d/pitch.py
@@ -399,7 +399,7 @@ class TalbotLineout:
 
         # shift peak to zero
         # x_shift = np.fft.fftshift(x_pad)
-        x_shift = x_fft[int(x_peak - crop_width):int(x_peak + crop_width)]
+        x_shift = np.fft.fftshift(x_fft[int(x_peak - crop_width):int(x_peak + crop_width)])
 
         # Nx1 = np.size(x_fft)
         #
@@ -426,8 +426,8 @@ class TalbotLineout:
         # remove edges just in case there's an issue there
         x_filt = x_filt[int(Nx/16):-int(Nx/16)]
         x_prime = x_prime[int(Nx/16):-int(Nx/16)]
-        # x_filt = x_filt[2:-2]
-        # x_prime = x_prime[2:-2]
+        #x_filt = x_filt[2:-2]
+        #x_prime = x_prime[2:-2]
 
         # polynomial fit for residual unwrapped phase
         px = np.polyfit(x_prime, np.unwrap(np.angle(x_filt)), 1)
