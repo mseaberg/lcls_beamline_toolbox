added beam amplitude fix when sampling changes as per Fresnel scaling theorem

lcls_beamline_toolbox/xraybeamline2d/beam1d.py
@@ -214,8 +214,10 @@ class Beam:
         self.global_z = beam_params['z_source'] + (self.zx + self.zy) / 2
 
         # initialize global angles
-        self.global_azimuth = np.copy(self.ax)
-        self.global_elevation = np.copy(self.ay)
+        # self.global_azimuth = np.copy(self.ax)
+        # self.global_elevation = np.copy(self.ay)
+        self.global_azimuth = 0.0
+        self.global_elevation = 0.0
 
         # initialize group delay
         self.group_delay = 0.0
@@ -251,6 +253,8 @@ class Beam:
         self.xhat = np.array([1,0,0])
         self.yhat = np.array([0,1,0])
         self.zhat = np.array([0,0,1])
+
+        self.rotate_nominal(delta_elevation=self.ay,delta_azimuth=self.ax)
 
         # define LCLS unit vectors
         self.x_nom = np.copy(self.xhat)
@@ -587,12 +591,20 @@ class Beam:
                 # scaled propagation
                 self.propagation(dz_remaining, z_eff_x, z_eff_y)
 
+                # get current beam sampling for adjustment of amplitude
+                dx_old = np.copy(self.dx)
+                dy_old = np.copy(self.dy)
+
                 # rescale coordinates based on magnification
                 self.rescale_x_noshift(mag_x)
                 self.rescale_y_noshift(mag_y)
 
                 # update beam center and radii of curvature
                 self.update_parameters(dz_remaining)
+
+                # adjust amplitude based on change in sampling
+                self.wavex *= self.dx/dx_old
+                self.wavey *= self.dy/dy_old
 
                 # return the wave
                 return self.wavex, self.wavey

lcls_beamline_toolbox/xraybeamline2d/optics1d.py
@@ -2142,7 +2142,7 @@ class Crystal(Mirror):
         if self.material == 'Si':
             self.crystal = materials.CrystalSi(hkl=self.hkl)
         elif self.material == 'diamond':
-            self.crystal = materials.CrystalDiamond(hkl=self.hkl)
+            self.crystal = materials.CrystalDiamond(hkl=self.hkl,d=0.8917,elements='C')
 
         # lattice spacing
         self.d = self.crystal.d * 1e-10
@@ -3429,6 +3429,8 @@ class PPM:
         self.cy = 0.0
         self.wx = 0.0
         self.wy = 0.0
+        self.sx = 0.0
+        self.sy = 0.0
         self.lambda0 = 0.0
         self.group_delay = 0
 
@@ -3453,6 +3455,8 @@ class PPM:
         self.cy = 0.0
         self.wx = 0.0
         self.wy = 0.0
+        self.sx = 0.0
+        self.sy = 0.0
         self.lambda0 = 0.0
 
     def beam_analysis(self, line_x, line_y):
@@ -3635,7 +3639,7 @@ class PPM:
         self.y_lineout = np.sum(self.profile, axis=1)
 
         # calculate centroids and beam widths
-        self.cx, self.cy, self.wx, self.wy, wx2, xy2 = self.beam_analysis(self.x_lineout, self.y_lineout)
+        self.cx, self.cy, self.wx, self.wy, self.sx, self.sy = self.beam_analysis(self.x_lineout, self.y_lineout)
 
         # get beam wavelength
         self.lambda0 = beam.lambda0