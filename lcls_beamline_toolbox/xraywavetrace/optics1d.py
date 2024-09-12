fix to crystal rotation

lcls_beamline_toolbox/xraywavetrace/optics1d.py
@@ -5665,10 +5665,12 @@ class Crystal(Mirror):
         # might want to do one axis at a time or change the order. Or could change the rotation
         # to rotate about the "unrotated" axes.
         # have checked the following with a diagram and it is correct
-        delta_ax = np.arcsin(np.sqrt(delta_k[0]**2+delta_k[2]**2))
+        # delta_ax = np.arcsin(np.sqrt(delta_k[0]**2+delta_k[2]**2))
         # delta_ax = np.arcsin(delta_k[0]/np.cos(self.beta0))
+        delta_ax = np.arcsin(delta_k[0])
         x_sign = np.sign(np.dot(np.cross(k_i, k_f_global), beam.yhat))
-        delta_ay = -np.arcsin(np.sqrt(delta_k[1]**2+delta_k[2]**2))
+        # delta_ay = -np.arcsin(np.sqrt(delta_k[1]**2+delta_k[2]**2))
+        delta_ay = -np.arcsin(delta_k[1])
         y_sign = np.sign(-np.dot(np.cross(k_i, k_f_global), beam.xhat))
         beam.rotate_beam(delta_ax=x_sign * np.abs(delta_ax), delta_ay=y_sign * np.abs(delta_ay))
 